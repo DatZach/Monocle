efa4d9554f39d5a088fe9e65297b3f31fdb40787
Zach Reedy <paraton@hotmail.com>
2013-04-07 01:39:44 -0400

    Initial commit

---
+++ Source/Fasm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Monocle
+{
+	class Fasm
+	{
+		private readonly string cachePath;
+		private readonly List<string> filenames; 
+
+		public Fasm()
+		{
+			filenames = new List<string>();
+
+			// Get a new temp directory
+			cachePath = CreateTemporaryPath();
+
+			// Set path so that FASM will work
+			Environment.SetEnvironmentVariable("PATH", Directory.GetCurrentDirectory());
+		}
+
+		public void Add(string value)
+		{
+			string filename = cachePath + "\\" + Path.GetRandomFileName().Split(new[] { '.' }).First() + ".asm";
+			filenames.Add(filename);
+
+			File.WriteAllText(filename, value);
+		}
+
+		public void Assemble(string filename)
+		{
+			Process process = new Process
+			{
+				StartInfo =
+				{
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					FileName = "fasm.exe",
+					Arguments = filenames.Aggregate((s, n) => s + " " + n) + " " + filename
+				}
+			};
+
+			process.Start();
+
+			string result = process.StandardOutput.ReadToEnd();
+
+			process.WaitForExit();
+
+			if (result.Select((c, i) => result.Substring(i)).Count(sub => sub.StartsWith("error")) != 0)
+				throw new Exception(result);
+				
+		}
+
+		private string CreateTemporaryPath()
+		{
+			string tempDirectory = Path.GetTempPath();
+			string tempExtension = Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
+			string path = Path.Combine(tempDirectory, tempExtension ?? "fasm");
+
+			Directory.CreateDirectory(path);
+
+			return path;
+		}
+	}
+}
---
+++ Source/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Monocle
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Fasm fasm = new Fasm();
+
+			fasm.Add(
+				";\n" +
+				";      console.asm\n" +
+				";      Write \"Hello, World!\" to console from assembly\n" +
+				";\n" +
+				"\n" +
+				"format PE\n" +
+				"entry EntryPoint\n" +
+				"\n" +
+				"include 'win32a.inc'\n" +
+				"\n" +
+				"StandardCallTest:\n" +
+				"       push    ebp\n" +
+				"       mov             ebp, esp\n" +
+				"       \n" +
+				"       invoke  GetStdHandle, 0xFFFFFFF5\n" +
+				"       invoke  WriteConsole, eax, [ebp + 8], [ebp + 12], [charsWritten], 0\n" +
+				"       \n" +
+				"       pop             ebp\n" +
+				"       ret             8\n" +
+				"\n" +
+				"EntryPoint:\n" +
+				"       push    17\n" +
+				"       push    message\n" +
+				"       call    StandardCallTest\n" +
+				"       \n" +
+				"       invoke  ExitProcess, 0\n" +
+				"\n" +
+				"message        db      'Hello, World!', 10, 13, 0\n" +
+				"charsWritten dd ?\n" +
+				"       \n" +
+				"data import\n" +
+				"       library kernel, 'KERNEL32.DLL',\\\n" +
+				"                       user, 'USER32.DLL'\n" +
+				"       \n" +
+				"       import kernel,\\\n" +
+				"               GetStdHandle, 'GetStdHandle',\\\n" +
+				"               WriteConsole, 'WriteConsoleA',\\\n" +
+				"               ExitProcess, 'ExitProcess'\n" +
+				"       \n" +
+				"       import user,\\\n" +
+				"               MessageBox, 'MessageBoxA'\n" +
+				"end data\n"
+			);
+
+			try
+			{
+				fasm.Assemble("testasm.exe");
+			}
+			catch(Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
